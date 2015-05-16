dfa6083b904ac36be5fedcf90d3b60b40b170409
Jennifer Jiyue Wang <jiyue@stanford.edu>
2015-05-15 22:34:52 -0700

    Initial Commit

---
+++ AR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AR
+//
+//  Created by Jiyue Wang on 5/15/15.
+//  Copyright (c) 2015 Jiyue Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AR
+//
+//  Created by Jiyue Wang on 5/15/15.
+//  Copyright (c) 2015 Jiyue Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
